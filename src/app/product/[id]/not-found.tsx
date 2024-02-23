d1d31250afa4dc8636bf3b2abe1d7d75e93d3e99
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-02-23 12:07:29 +0100

    add product page

---
+++ src/app/product/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductNotFoundPage = () => {
+  return (
+    <div>
+      <h1>Product not found</h1>
+    </div>
+  );
+};
+
+export default ProductNotFoundPage;
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,90 @@
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import { db } from '@/lib/server/db';
+import { products } from '@/lib/server/tables';
+import { eq } from 'drizzle-orm';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import React from 'react';
+
+import { z } from 'zod';
+
+const paramsSchema = z.object({
+  id: z.string().length(36),
+});
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+const ProductPage = async ({ params }: Props) => {
+  const result = paramsSchema.safeParse(params);
+
+  // since we know that the id should contain a valid UUID, we can safely throw a 404 if it's shorter than 36 characters, which saves us a db query
+  if (!result.success) {
+    return notFound();
+  }
+
+  const { id } = result.data;
+
+  const product = await db.query.products.findFirst({
+    where: eq(products.id, id),
+    with: {
+      images: true,
+      tags: true,
+      reviews: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  const {
+    title,
+    description,
+    priceInCents,
+    discountInCents,
+    images,
+    tags,
+    reviews,
+    id: productId,
+  } = product;
+
+  return (
+    <Card className="mx-auto max-w-2xl bg-white p-4">
+      <div className="mb-6">
+        <Image
+          alt="Vanilla Perfume"
+          className="h-auto w-full rounded-lg"
+          height="300"
+          src={images[0].url}
+          style={{
+            aspectRatio: '300/300',
+            objectFit: 'cover',
+          }}
+          width="300"
+        />
+      </div>
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-gray-600">{description}</p>
+      </div>
+      <div className="mb-4 flex items-baseline space-x-2">
+        <span className="text-xl font-semibold">
+          ${(priceInCents / 10000).toFixed(2)}
+        </span>
+        <span className="text-sm text-gray-500 line-through">
+          ${(discountInCents / 10000).toFixed(2)}
+        </span>
+      </div>
+      <div className="flex space-x-4">
+        <Button>Add to Cart</Button>
+      </div>
+    </Card>
+  );
+};
+
+export default ProductPage;
