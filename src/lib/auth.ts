0bc935ce92577c92bf2ef8b8bc2df75b0c7d45e1
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-03-26 13:40:47 +0100

    update next js

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,12 @@
 'use server';
 
-import { cache } from 'react';
 import { getSessionCookie } from './server/auth/cookies';
 import { getSessionDetails } from './server/services/sessionService';
 
-export const validateRequest = cache(async () => {
+export const validateRequest = async () => {
   const sessionId = getSessionCookie();
 
   const result = await getSessionDetails(sessionId);
 
   return result;
-});
+};
