b5db5117799cbae4e1c8c52d43375d681729dea5
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-03-30 11:31:59 +0100

    Add cmdk package and handle hot reloading in dbConnection.ts

--- src/lib/server/dbConnection.ts
+++ src/lib/server/dbConnection.ts
@@ -3,27 +3,43 @@ import mysql from 'mysql2/promise';
 
 let cachedConnection: mysql.Connection | null = null;
 
+// Hot reloading in nextjs dev mode will cause this file to
+// be reloaded and the connections to mysql to pile up
 export const createDbConnection = async (): Promise<mysql.Connection> => {
-  if (cachedConnection) {
-    return cachedConnection;
-  }
-
-  try {
-    cachedConnection = await mysql.createConnection({
-      host: process.env.DATABASE_HOST,
-      user: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: 'anshin',
-      port: Number(process.env.DATABASE_PORT), // 💡 3307 when using docker and 3306 for deployment
-    });
-    return cachedConnection;
-  } catch (error) {
-    const isDev = process.env.NODE_ENV === 'development';
-    if (isDev) {
-      throw new Error(
-        `Error connecting to database, did you remember to start docker? ${error}`
-      );
+  if (process.env.NODE_ENV !== 'production') {
+    if (cachedConnection) {
+      return cachedConnection;
     }
-    throw new Error('Error connecting to database');
+
+    try {
+      cachedConnection = await mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: 'anshin',
+        port: Number(process.env.DATABASE_PORT), // 💡 3307 when using docker and 3306 for deployment
+      });
+      return cachedConnection;
+    } catch (error) {
+      const isDev = process.env.NODE_ENV === 'development';
+      if (isDev) {
+        throw new Error(
+          `Error connecting to database, did you remember to start docker? ${error}`
+        );
+      }
+      throw new Error('Error connecting to database');
+    }
+  } else {
+    try {
+      return await mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: 'anshin',
+        port: Number(process.env.DATABASE_PORT), // 💡 3307 when using docker and 3306 for deployment
+      });
+    } catch (error) {
+      throw new Error('Error connecting to database');
+    }
   }
 };
