28ea4de2b13e08122c9271c284c35316f2dfd63c
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-04-07 12:46:39 +0200

    update alignment

--- src/app/(searchLayout)/page.tsx
+++ src/app/(searchLayout)/page.tsx
@@ -3,7 +3,6 @@ import { getSessionCookie } from '@/lib/server/auth/cookies';
 import { getAllProducts } from '@/lib/server/services/productService';
 import { HeroSection } from '../../components/HeroSection';
 import { SearchPage } from './SearchPage';
-import { LoginSuccessToast } from './LoginSuccessToast';
 
 type Props = {
   searchParams?: {
@@ -25,7 +24,7 @@ export default async function Home({ searchParams }: Props) {
   return (
     <div className="flex w-full flex-col items-center">
       <HeroSection />
-      <h3 className="self-start pb-4 pt-14 text-xl sm:pt-28">All Products</h3>
+      <h3 className="self-start pb-4 pt-14 text-xl">All Products</h3>
       <section className="flex w-full flex-col">
         <div className="grid w-full gap-4 xs:grid-cols-2 sm:grid-cols-2 sm:gap-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-8">
           {allProducts.map((product) => (
